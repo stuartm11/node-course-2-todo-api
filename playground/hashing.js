87596243865628e55629154c16603309d7432dd2
Stuart Mehrens <stuart.mehrens@gmail.com>
2018-03-24 19:13:31 -0700

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,31 @@
-const {SHA256} = require('crypto-js');
-const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-console.log(jwt.verify(token, '1123abc'));
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+
+var hashedPassword = '$2a$10$ucm4M/fNCtCNId0XF99vguIi5yfT8mrO55gcAymQNoCaBd.30ZzWS';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    if (res) {
+        console.log(res);
+    }
+});
+// const {SHA256} = require('crypto-js');
+// const jwt = require('jsonwebtoken');
+//
+// var data = {
+//     id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// console.log(jwt.verify(token, '1123abc'));
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
